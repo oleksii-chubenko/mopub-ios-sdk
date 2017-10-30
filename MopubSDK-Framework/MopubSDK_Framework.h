c5f7e3fe7b5853d2d519aa8101a89104b9422a4f
Oleksii Chubenko <oleksii@timecode.co>
2017-10-30 18:28:14 +0200

    Added test wrapping into framework (not tested)

---
+++ MopubSDK-Framework/MopubSDK_Framework.h
@@ -0,0 +1,19 @@
+//
+//  MopubSDK_Framework.h
+//  MopubSDK-Framework
+//
+//  Created by Oleksii Chubenko on 10/27/17.
+//  Copyright © 2017 MoPub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MopubSDK_Framework.
+FOUNDATION_EXPORT double MopubSDK_FrameworkVersionNumber;
+
+//! Project version string for MopubSDK_Framework.
+FOUNDATION_EXPORT const unsigned char MopubSDK_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MopubSDK_Framework/PublicHeader.h>
+
+
